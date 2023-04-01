b5c022c8dfbfdf0fa1d2098c06a254e289339017
Martín Araujo <mnaheavy@gmail.com>
2023-04-01 00:04:10 -0300

    editValRoute

--- src/routes/getValues.js
+++ src/routes/getValues.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const getValues = require("../controllers/values/getValues");
+const editValues = require("../controllers/values/editValues");
 const router = Router();
 
 router.get("/values", getValues);
+router.put("/values/:id", editValues);
 
 module.exports = router;
