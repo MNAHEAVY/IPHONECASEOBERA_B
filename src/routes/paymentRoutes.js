e12c83b69362c4f7ba3dd0844c1189d1b332ef78
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2023-12-14 11:06:03 -0300

    fix

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,13 +1,8 @@
 const { Router } = require("express");
-const {
-  createPreference,
-  // createFeedback,
-} = require("../controllers/MercadoPago/MercadoPago");
+const { createPreference } = require("../controllers/MercadoPago/MercadoPago");
 
 const router = Router();
 
 router.post("/create_preference", createPreference);
-
-//router.get("/feedback", createFeedback);
 
 module.exports = router;
