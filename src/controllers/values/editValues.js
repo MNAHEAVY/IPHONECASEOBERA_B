b46264cd6c28b9d8998ae342920de80348a0b020
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2026-05-28 19:59:27 -0300

    refactor: simplify updateValues function by directly using req.body for updates

--- src/controllers/values/editValues.js
+++ src/controllers/values/editValues.js
@@ -2,31 +2,26 @@ const Values = require("../../models/values");
 
 const updateValues = async (req, res) => {
   const { id } = req.params;
-  const { dolar, margen, flete, fleteLocal, obercoins, costosDeEnvio, iva, mp, rentas } =
-    req.body;
 
   try {
+    // Al pasar req.body directamente, Mongoose solo actualizará los campos que envías.
+    // { new: true, runValidators: true } asegura que devuelva el objeto nuevo y ejecute las validaciones del Modelo.
     const updatedValues = await Values.findByIdAndUpdate(
       id,
-      {
-        dolar,
-        margen,
-        flete,
-        fleteLocal,
-        obercoins,
-        costosDeEnvio,
-        iva,
-        mp,
-        rentas,
-      },
-      { new: true },
+      { $set: req.body },
+      { new: true, runValidators: true },
     );
+
     if (!updatedValues) {
       return res.status(404).json({ message: "Values not found" });
     }
+
     return res.status(200).json(updatedValues);
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    console.error("Backend Error:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
 
